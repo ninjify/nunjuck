461273ca595ef952612bf5c9b0494dd2771c6432
Milan Felix Šulc <rkfelix@gmail.com>
2016-12-12 21:11:16 +0100

    OutputHandler: finalize

--- bin/tester-setup.php
+++ bin/tester-setup.php
@@ -1,50 +1,8 @@
 <?php
 
-use Tester\Dumper;
-use Tester\Runner\Output\ConsolePrinter;
+use Ninjify\Tester\OutputHandler\BetterConsolePrinter;
 use Tester\Runner\Runner;
 
 /** @var Runner $runner */
 $runner->outputHandlers = [];
-$runner->outputHandlers[] = new class ($runner) extends ConsolePrinter
-{
-
-	public function begin()
-	{
-		ob_start();
-		parent::begin();
-		echo rtrim(ob_get_clean()) . "\n\n";
-	}
-
-	public function result($testName, $result, $message)
-	{
-		$outputs = [
-			Runner::PASSED => Dumper::color('green', '✔ ' . $testName),
-			Runner::SKIPPED => Dumper::color('olive', 's ' . $testName) . "($message)",
-			Runner::FAILED => Dumper::color('red', '✖ ' . $testName) . "\n" . $this->indent($message, 3) . "\n",
-		];
-		echo $this->indent($outputs[$result], 2) . PHP_EOL;
-	}
-
-	public function end()
-	{
-		ob_start();
-		parent::end();
-		echo "\n" . trim(ob_get_clean()) . "\n";
-	}
-
-	private function indent($message, $spaces)
-	{
-		if ($message) {
-			$result = '';
-			foreach (explode(PHP_EOL, $message) as $line) {
-				$result .= str_repeat(' ', $spaces) . $line . PHP_EOL;
-			}
-
-			return rtrim($result, PHP_EOL);
-		}
-
-		return $message;
-	}
-
-};
+$runner->outputHandlers[] = new BetterConsolePrinter($runner);
---
+++ src/OutputHandler/BetterConsolePrinter.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Ninjify\Tester\OutputHandler;
+
+use Tester\Dumper;
+use Tester\Runner\Output\ConsolePrinter;
+use Tester\Runner\Runner;
+
+/**
+ * @copyright Martin Zlamal <http://zlml.cz/tester-vlastni-output-handler>
+ */
+class BetterConsolePrinter extends ConsolePrinter
+{
+
+	/**
+	 * Display end of single test
+	 *
+	 * @return void
+	 */
+	public function begin()
+	{
+		ob_start();
+		parent::begin();
+		echo rtrim(ob_get_clean()) . "\n\n";
+	}
+
+	/**
+	 * Display result of single test
+	 *
+	 * @param string $testName
+	 * @param int $result
+	 * @param string $message
+	 * @return void
+	 */
+	public function result($testName, $result, $message)
+	{
+		$outputs = [
+			Runner::PASSED => Dumper::color('green', '✔ ' . $testName),
+			Runner::SKIPPED => Dumper::color('olive', 's ' . $testName) . "($message)",
+			Runner::FAILED => Dumper::color('red', '✖ ' . $testName) . "\n" . $this->indent($message, 3) . "\n",
+		];
+		echo $this->indent($outputs[$result], 2) . PHP_EOL;
+	}
+
+	/**
+	 * Display end of single test
+	 *
+	 * @return void
+	 */
+	public function end()
+	{
+		ob_start();
+		parent::end();
+		echo "\n" . trim(ob_get_clean()) . "\n";
+	}
+
+	/**
+	 * @param string $message
+	 * @param int $spaces
+	 * @return string
+	 */
+	private function indent($message, $spaces)
+	{
+		if ($message) {
+			$result = '';
+			foreach (explode(PHP_EOL, $message) as $line) {
+				$result .= str_repeat(' ', $spaces) . $line . PHP_EOL;
+			}
+
+			return rtrim($result, PHP_EOL);
+		}
+
+		return $message;
+	}
+
+}
